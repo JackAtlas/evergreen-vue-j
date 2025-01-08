15a9fb5c2be44893d061069b91801127092a6950
JackAtlas <jackatlas8864@gmail.com>
2025-01-08 09:13:42 +0800

    Icon

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <i class="ev-icon" v-bind="$attrs" :class="{ [`ev-icon--${type}`]: type }" :style="customStyles">
+    <font-awesome-icon v-bind="filteredProps" />
+  </i>
+</template>
+
+<script setup lang="ts">
+import { omit } from 'radash'
+import type { IconProps } from './types.ts'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { computed } from 'vue'
+defineOptions({
+  name: 'ev-icon',
+  inheritAttrs: false,
+})
+const props = defineProps<IconProps>()
+const filteredProps = computed(() => omit(props, ['type', 'color']))
+const customStyles = computed(() => {
+  return props.color ? { color: props.color } : {}
+})
+</script>
