951ea826a57a739975e5b11804e890aa2c89d15e
JackAtlas <jackatlas8864@gmail.com>
2025-01-14 17:27:21 +0800

    style

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-
+import './styles/index.css'
 import App from './App.vue'
 
 library.add(fas)
