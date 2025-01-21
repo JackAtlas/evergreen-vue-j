d24a848b5149ffdcb665604ac64bf8bce6c4c065
JackAtlas <jackatlas8864@gmail.com>
2025-01-21 13:59:00 +0800

    tooltip test

--- src/components/Tooltip/Tooltip.test.tsx
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -11,7 +11,7 @@ describe('Tooltip.vue', () => {
   test('basic tooltip', async () => {
     const wrapper = mount(
       () => (
-        <Tooltip content="hello tooltip" trigger="click" onVisibleChange={onVisibleChange}>
+        <Tooltip content="hello tooltip" trigger="click" onVisible-change={onVisibleChange}>
           <button id="trigger">Trigger</button>
         </Tooltip>
       ),
@@ -28,6 +28,33 @@ describe('Tooltip.vue', () => {
     expect(wrapper.get('.ev-tooltip__popper').text()).toBe('hello tooltip')
     expect(onVisibleChange).toHaveBeenCalledWith(true)
     triggerArea.trigger('click')
+    await vi.runAllTimers()
+    expect(wrapper.find('.ev-tooltip__popper').exists()).toBeFalsy()
+    expect(onVisibleChange).toHaveBeenLastCalledWith(false)
+  })
+  test('click outside', async () => {
+    const wrapper = mount(
+      () => (
+        <div>
+          <div id="outside"></div>
+          <Tooltip content="hello tooltip" trigger="click" onVisible-change={onVisibleChange}>
+            <button id="trigger">Trigger</button>
+          </Tooltip>
+        </div>
+      ),
+      { attachTo: document.body },
+    )
+
+    const triggerArea = wrapper.find('#trigger')
+    expect(triggerArea.exists()).toBeTruthy()
+    expect(wrapper.find('.ev-tooltip__popper').exists()).toBeFalsy()
+
+    triggerArea.trigger('click')
+    await vi.runAllTimers()
+    expect(wrapper.find('.ev-tooltip__popper').exists()).toBeTruthy()
+    expect(wrapper.get('.ev-tooltip__popper').text()).toBe('hello tooltip')
+    expect(onVisibleChange).toHaveBeenCalledWith(true)
+    wrapper.get('#outside').trigger('click')
     await vi.runAllTimers()
     expect(wrapper.find('.ev-tooltip__popper').exists()).toBeFalsy()
     expect(onVisibleChange).toHaveBeenLastCalledWith(false)
