f145e35357361b622d11b575dbdf95f5e21146f4
JackAtlas <jackatlas8864@gmail.com>
2025-01-26 21:08:20 +0800

    docs menu

---
+++ docs/components/menu.md
@@ -0,0 +1,12 @@
+---
+title: Menu
+description: Menu 组件的文档
+---
+
+# Menu 列表
+
+有序展示信息。
+
+## 基础列表
+
+<preview path="" title="基础列表"></preview>
---
+++ docs/demo/Menu/Basic-Menu.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Menu from '../../../src/components/Menu/Menu.vue'
+import MenuItem from '../../../src/components/Menu/MenuItem.vue'
+</script>
+
+<template>
+  <Menu>
+    <MenuItem>Share</MenuItem>
+    <MenuItem>Move</MenuItem>
+    <MenuItem>Rename</MenuItem>
+  </Menu>
+</template>
